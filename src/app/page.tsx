ca8355004f6f4ce539ce57247939190c80624816
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-08-21 22:02:52 +0530

    Removed custom component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
-import PageCard from "@/components/PageCard";
 
 const services = ["Page Config", "Page Section", "Service Config"];
 
 export default async function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-full">
-      <span className="text-xl m-4">Services</span>
+      <span className="text-2xl m-8">Services</span>
       <div className="flex justify-center">
         {services.length !== 0 &&
           services.map((name, id) => (
-            <Link href={`/${name.replaceAll(" ", "_").toLowerCase()}`}>
-              <PageCard key={id} name={name} />
+            <Link href={`/${name.replaceAll(" ", "_").toLowerCase()}`} key={id}>
+              <div className="flex w-44 bg-white p-4 rounded-md mx-2">
+                <span className="text-black">{name}</span>
+              </div>
             </Link>
           ))}
       </div>
