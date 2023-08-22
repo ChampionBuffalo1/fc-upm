f6af172e131b4d9eafcb001f0053d29f7dfbb01d
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-08-22 16:30:31 +0530

    Fixed the layout shift

--- src/app/page_config/PageCard.tsx
+++ src/app/page_config/PageCard.tsx
@@ -1,28 +1,31 @@
-import clsx from "clsx";
 import Link from "next/link";
 import { Pencil } from "lucide-react";
 import { PageConfig } from "@/types/db";
 
-export default function PageCard({
-  id,
-  name,
-  className,
-}: PageConfig & { className?: string }) {
+export default function PageCard({ id, name }: PageConfig) {
   return (
-    <div className={clsx("flex w-44 bg-white p-4 rounded-md mx-2", className)}>
-      <span className="text-black">{name}</span>
-      <div className="w-full flex flex-row-reverse items-center">
-        <Link
-          href={{
-            pathname: "/page_config/edit",
-            query: {
-              id,
-            },
-          }}
-        >
-          <Pencil size={16} className="text-red-400" />
-        </Link>
-      </div>
+    <div className="bg-white mx-2 p-4 flex items-center rounded-md justify-between">
+      <Link
+        href={{
+          pathname: "/page_config/service",
+          query: {
+            name,
+          },
+        }}
+      >
+        <span className="text-black">{name}</span>
+      </Link>
+      <Link
+        href={{
+          pathname: "/page_config/edit",
+          query: {
+            id,
+          },
+        }}
+        className="px-2 "
+      >
+        <Pencil size={16} className="text-red-400" />
+      </Link>
     </div>
   );
 }
