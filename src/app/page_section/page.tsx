c82865b5807744053db429776d8eb6ee342b2378
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-08-23 16:10:28 +0530

    Displaying page_section

--- src/app/page_section/page.tsx
+++ src/app/page_section/page.tsx
@@ -1,5 +1,10 @@
 import mysql from "@/lib/mysql";
+import PageCard from "@/components/PageCard";
 import { PageSection } from "@/types/db";
+import CreateButton from "@/components/CreateButton";
+
+const query =
+  "SELECT ps.id, priority, active, name FROM `page_section` ps LEFT JOIN `page_config` pc ON pc.id = ps.page_config_id";
 
 export default async function PageConfigServices({
   searchParams,
@@ -7,20 +12,36 @@ export default async function PageConfigServices({
   searchParams: { id?: string };
 }) {
   const id = searchParams.id;
+  const data = (await mysql.query(
+    id ? `${query} WHERE \`page_config_id\` = ?` : query,
+    [id]
+  )) as (PageSection & { name: string })[];
 
-  if (id) {
-    const data = (await mysql.query(
-      "SELECT * FROM `page_section` WHERE `page_config_id` = ?",
-      [id]
-    )) as PageSection[];
-    return <pre>{JSON.stringify(data, null, 2)}</pre>;
-  }
+  if (data.length === 0)
+    return (
+      <span className="text-red-500 text-xl">
+        No sections found {id && `with ID ${id}`}
+      </span>
+    );
 
-  const data = await mysql.query("SELECT * FROM `page_section`");
   return (
-    <div>
-      {id}
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+    <div className="flex flex-col justify-center items-center h-full">
+      <span className="text-xl mb-2">
+        {id ? data[0]?.name : "All"} Sections
+      </span>
+      <div className="justify-center flex flex-wrap">
+        {data.map((obj, key) => (
+          <PageCard
+            key={key}
+            pid={obj.id}
+            name={id ? `Section ${obj.id}` : `${obj.name} Section ${obj.id}`}
+            clickHref="/service_config"
+            editHref="/page_section/edit"
+            className="mt-2"
+          />
+        ))}
+        <CreateButton href="/page_section/create" className="mt-2" />
+      </div>
     </div>
   );
 }
