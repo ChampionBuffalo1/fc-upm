7e89e3f526b3cebc67f66a8d011ba2d69463824f
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-08-23 23:20:02 +0530

    Using custom form format

--- src/app/page_section/edit/form.tsx
+++ src/app/page_section/edit/form.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import dayjs, { Dayjs } from "dayjs";
+import { Dayjs } from "dayjs";
 import { PageSection } from "@/types/db";
-import { RequiredMark } from "@/types/util";
 import { useRouter } from "next/navigation";
-import { useCallback, useState } from "react";
-import { Button, DatePicker, Form, Input } from "antd";
+import { useCallback, useMemo } from "react";
+import JsonForm, { FormField } from "@/components/Forms";
 
 export default function EditForm({
   id,
@@ -16,18 +15,46 @@ export default function EditForm({
   page_config_id,
 }: PageSection) {
   const router = useRouter();
-  const [form] = Form.useForm();
-  const [requiredMark, setRequiredMarkType] =
-    useState<RequiredMark>("optional");
-
-  const onRequiredTypeChange = ({
-    requiredMarkValue,
-  }: {
-    requiredMarkValue: RequiredMark;
-  }) => {
-    setRequiredMarkType(requiredMarkValue);
-  };
-
+  const fields: FormField = useMemo(
+    () => [
+      {
+        name: "ca-picker",
+        label: "Created At",
+        required: "Please input created_at!",
+        type: "date",
+        initialValue: created_at,
+      },
+      {
+        name: "ua-picker",
+        label: "Updated At",
+        required: "Please input updated_at!",
+        type: "date",
+        initialValue: updated_at,
+      },
+      {
+        name: "active",
+        label: "Active",
+        required: "Please input active!",
+        type: "number",
+        initialValue: active,
+      },
+      {
+        name: "priority",
+        label: "Priority",
+        required: "Please input priority!",
+        type: "number",
+        initialValue: priority,
+      },
+      {
+        type: "number",
+        name: "page_config_id",
+        label: "Page Config Id",
+        required: "Please input page_config_id!",
+        initialValue: page_config_id,
+      },
+    ],
+    []
+  );
   const handleSubmit = useCallback(
     (value: {
       "ca-picker": Dayjs;
@@ -59,98 +86,7 @@ export default function EditForm({
   return (
     <div>
       <span className="text-2xl">Editing Page Section</span>
-      <Form
-        className="w-full pt-4"
-        form={form}
-        layout="vertical"
-        onFinish={handleSubmit}
-        requiredMark={requiredMark}
-        onValuesChange={onRequiredTypeChange}
-        initialValues={{ requiredMarkValue: requiredMark }}
-      >
-        <Form.Item
-          name="ca-picker"
-          label={<label className="text-white"> Created At</label>}
-          rules={[
-            {
-              type: "object" as const,
-              required: true,
-              message: "Please select time!",
-            },
-          ]}
-          initialValue={dayjs(created_at)}
-        >
-          <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
-        </Form.Item>
-
-        <Form.Item
-          name="ua-picker"
-          label={
-            <div className="flex flex-col">
-              <label className="text-white"> Updated At</label>
-              <em className="text-gray-500 hover:text-gray-200 text-xs">
-                This will be updated to current time when you submit
-              </em>
-            </div>
-          }
-          rules={[
-            {
-              type: "object" as const,
-            },
-          ]}
-          initialValue={dayjs(updated_at)}
-        >
-          <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
-        </Form.Item>
-
-        <Form.Item
-          name="priority"
-          label={<label className="text-white">Priority</label>}
-          rules={[
-            {
-              required: true,
-              message: "Priority must be a number",
-              pattern: new RegExp(/^[0-9]+$/),
-            },
-          ]}
-          initialValue={priority}
-        >
-          <Input placeholder="priority" />
-        </Form.Item>
-        <Form.Item
-          name="active"
-          label={<label className="text-white">Active</label>}
-          rules={[
-            {
-              required: true,
-              message: "Active must be a number",
-              pattern: new RegExp(/^[0-9]+$/),
-            },
-          ]}
-          initialValue={active}
-        >
-          <Input placeholder="active" />
-        </Form.Item>
-        <Form.Item
-          name="page_config_id"
-          label={<label className="text-white">Page Config Id</label>}
-          rules={[
-            {
-              required: true,
-              message: "Each section must be associated with a page config",
-              pattern: new RegExp(/^[0-9]+$/),
-            },
-          ]}
-          initialValue={page_config_id}
-        >
-          <Input placeholder="Page config Id" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+      <JsonForm fields={fields} submit={handleSubmit} key={id} />
     </div>
   );
 }
