7e23aaccac191e2dbb7d17d7ca6f17c48f976af8
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-08-24 11:34:19 +0530

    Added service_config/create

---
+++ src/app/service_config/create/page.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import { Dayjs } from "dayjs";
+import { useRouter } from "next/navigation";
+import { useCallback } from "react";
+import JsonForm, { FormField } from "@/components/Forms";
+
+export default function CreatePage() {
+  const router = useRouter();
+
+  const handleSubmit = useCallback(
+    (value: Record<string, Dayjs | string>) =>
+      fetch("/api/service_config", {
+        method: "PUT",
+        body: JSON.stringify(value),
+      }).then(() => {
+        router.refresh();
+        router.back();
+      }),
+    [router]
+  );
+
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      <span className="text-xl mb-8">New Service</span>
+      <div>
+        <JsonForm fields={fields} submit={handleSubmit} layout="horizontal" />
+      </div>
+    </div>
+  );
+}
+
+const fields: FormField = [
+  {
+    type: "date",
+    required: "This is a required field",
+    name: "ca-picker",
+    label: "Created At",
+  },
+  {
+    type: "date",
+    required: "This is a required field",
+    name: "ua-picker",
+    label: "Updated At",
+  },
+  {
+    name: "name",
+    label: "Name",
+    type: "string",
+    required: "Name must be provided",
+    placeholder: "Service Name",
+  },
+  {
+    type: "number",
+    name: "priority",
+    label: "Priority",
+    placeholder: "Service Priority",
+    required: "Priority is required",
+  },
+  {
+    type: "number",
+    name: "active",
+    label: "Active",
+    placeholder: "Active state",
+    required: "This field is required",
+  },
+  {
+    type: "string",
+    name: "app_version",
+    label: "App Version",
+    placeholder: "App Version",
+    required: "This field is required",
+  },
+  {
+    type: "string",
+    name: "base_action",
+    label: "Base Action",
+    placeholder: "Base Action",
+    required: "This field is required",
+  },
+  {
+    type: "string",
+    name: "static_info",
+    label: "Static Info",
+    placeholder: "Static Info",
+    required: "This field is required",
+  },
+  {
+    type: "string",
+    name: "fallback_info",
+    label: "Fallback Info",
+    placeholder: "Fallback Info",
+    required: "This field is required",
+  },
+  {
+    type: "string",
+    name: "content_type",
+    label: "Content Type",
+    placeholder: "Content Type",
+    required: "This field is required",
+  },
+  {
+    type: "string",
+    name: "content_structure",
+    label: "Content Structure",
+    placeholder: "Content Structure",
+    required: "This field is required",
+  },
+  {
+    type: "string",
+    name: "base_activity_name",
+    label: "Base Activity Name",
+    placeholder: "Base Activity Name",
+    required: "This field is required",
+  },
+];
