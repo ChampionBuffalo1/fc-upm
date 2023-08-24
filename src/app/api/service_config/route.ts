13ead242bb9085896ad66720480fe7fdd5e7b9a7
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-08-24 13:13:41 +0530

    Finish the Update route

---
+++ src/app/api/service_config/route.ts
@@ -0,0 +1,71 @@
+import mysql from "@/lib/mysql";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PUT(req: NextRequest) {
+  const body = await req.json();
+  const db = (await mysql.query(
+    "INSERT INTO `service_config`(created_at, updated_at, name, priority, active, app_version, base_action, static_info, fallback_info, content_type, content_structure, base_activity_name) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    [
+      new Date(body.created_at),
+      new Date(body.updated_at),
+      body.name,
+      body.priority,
+      body.active,
+      body.app_version,
+      body.base_action,
+      body.static_info,
+      body.fallback_info,
+      body.content_type,
+      body.content_structure,
+      body.base_activity_name,
+    ]
+  )) as { insertId: number };
+
+  return NextResponse.json({
+    data: {
+      id: db.insertId,
+    },
+  });
+}
+
+const allowedKeys = [
+  "name",
+  "created_at",
+  "update_at",
+  "priority",
+  "active",
+  "app_version",
+  "base_action",
+  "static_info",
+  "fallback_info",
+  "content_type",
+  "content_structure",
+  "base_activity_name",
+];
+export async function PATCH(req: NextRequest) {
+  const body = await req.json();
+  if (!body.id)
+    return NextResponse.json({
+      message: "id is required",
+    });
+
+  const validKeys = Object.keys(body).filter((k) => allowedKeys.includes(k));
+  if (validKeys.length === 0)
+    return NextResponse.json({
+      message: "no valid keys",
+    });
+
+  const sqlQuery = `UPDATE \`service_config\` SET ${validKeys
+    .map((k) => `${k} = ?`)
+    .join(", ")} WHERE id = ?`;
+
+  const values = validKeys.map((k) =>
+    k.endsWith("_at") ? new Date(body[k]) : body[k]
+  );
+  await mysql.query(sqlQuery, [...values, body.id]);
+  return NextResponse.json({
+    data: {
+      id: body.id,
+    },
+  });
+}
